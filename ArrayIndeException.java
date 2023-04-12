befe4a6a45df990aae8fcd0a5b9462a2c136355b
Jeeva022000 <130529885+Jeeva022000@users.noreply.github.com>
2023-04-12 10:17:21 +0530

    Update ArrayIndeException.java

--- ArrayIndeException.java
+++ ArrayIndeException.java
@@ -3,7 +3,7 @@ package com.onesoft.Exeptions;
 public class ArrayIndeException {
 	public static void main(String[] args) {
 		try {
-			int[] a = {1,2,3};
+			int[] a = {1,2,3,5,6};
 			for(int i=0;i<a.length;i++) {
 				System.out.println(a[4]);
 			}
@@ -12,7 +12,7 @@ public class ArrayIndeException {
 				System.out.println(e);
 			}
 			finally {
-			System.out.println("invalid");
+			System.out.println("invalid data");
 			}
 	}
 	}
